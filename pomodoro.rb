1cefa0733394bc4c1ec9f00f71f4b999f58547ce
Nathan <BuddytheRat@gmail.com>
2015-07-15 17:00:33 -0500

    time displayed as string

--- pomodoro.rb
+++ pomodoro.rb
@@ -15,18 +15,27 @@ class Timer
     @end_time = @start_time + @seconds
     @running = true
     Thread.new {
-      while Time.now < @end_time
+      while Time.now < @end_time && @running
         sleep 1
+        system('cls')
+        puts time_to_s + ' | ' + percent
       end
       time_up
     }
   end
+
+  def time_to_s
+    minutes = (((@end_time - Time.now) / 60) % 60).to_i.to_s.rjust(2, '0')
+    seconds = ((@end_time - Time.now) % 60).to_i.to_s.rjust(2, '0')
+    "#{minutes}:#{seconds}"
+  end    
 
   def percent
     "#{((Time.now - @start_time)/(@end_time - @start_time)*100).to_i}%"
   end
 
   def time_up
+    system('cls')
     puts "It's Over!"
     Sound.play('alarm.wav')
     @running = false
@@ -37,9 +46,8 @@ class Timer
   end
 end
 
-timer = Timer.new(0.1)
+timer = Timer.new(0.2)
 timer.start
 while timer.running?
-  system('cls')
-  puts timer.percent
+  
 end
